b7914bc9eceff8a74e4233d2eb893b9de4e9c23b
German Quesada <ga.quesada.solis@gmail.com>
2019-10-12 09:50:35 -0600

    Muestreo final
    Se muestrea la funcion coseno para los datos a procesar para los diferentes casos. Tanto 128, 64 y 32 muestras

--- archivoParaDiscretizarFuncion.m
+++ archivoParaDiscretizarFuncion.m
@@ -1,11 +1,39 @@
+
+%El presente codigo obtiene los datos para los tres casos a desarrollar.
+%En los tres casos se muestrea una funcion seno 
+
+
+
+%El primer caso tiene 32 muestras y un periodo de muestro de 0.4
+n1=2^5;
+dt1=0.4;
+t1=(0:n1-1)*dt1;
+x1=cos(t1) ;
+
+
+%El segundo caso tiene 64 muestras y un periodo de muestro de 0.2
+n2=2^6;
+dt2=0.2;
+t2=(0:n2-1)*dt2;
+x2=cos(t2) ;
+
+
+%El tercer caso tiene 128 muestras y un periodo de muestro de 0.1
+n3=2^7;
+dt3=0.1;
+t3=(0:n3-1)*dt3;
+x3=cos(t3) ;
+
+
+
+
 %Para obtener una matriz con valores discretizados de una función
-n=2^10;  %Numero de datos
-dt=0.4;  %  1/dt= Frecuencia de muestreo
-t=(0:n-1)*dt; %vector de tiempos
-x=cos(t) ; %  Función
+%n=2^10;  %Numero de datos
+%dt=0.4;  %  1/dt= Frecuencia de muestreo
+%t=(0:n-1)*dt; %vector de tiempos
+%x=cos(t) ; %  Función
 
 %Se pueden pasar datos a un archivo .txt para utilizarlos en NASM.
-
 
 %Resultado aplicado a función coseno
 
